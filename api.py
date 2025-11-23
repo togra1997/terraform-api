4519343d6fe04d023bdcfdd759557f458672cafc
togura <>
2025-11-23 15:48:53 +0000

    CORSミドルウェアの設定を環境変数に基づいて更新し、dotenvを使用して環境変数を読み込むように変更

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+import os
+
+import dotenv
 import uvicorn
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
@@ -7,12 +10,16 @@ from fastapi.responses import JSONResponse
 from src.api.endpoint.proxmox import router as proxmox_router
 from src.api.endpoint.terraform import router
 
+dotenv.load_dotenv()
+allow_origins = os.getenv("ALLOW_ORIGINS")
+
+
 app = FastAPI()
 app.include_router(router)
 app.include_router(proxmox_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[allow_origins],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -24,5 +31,9 @@ async def handler(request: Request, exc: RequestValidationError):
     return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
-if __name__ == "__main__":
+def main():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
